063e014575c7c448b6f81bf5c33ab23136fdfa85
iqbaldirmansyah06 <iqbaldirmansyah06@gmail.com>
2024-10-02 23:42:54 +0700

    fix

---
+++ database/migrations/2024_10_02_063053_create_transaction_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaction_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onDelete('cascade');
+            $table->timestamps();
+
+            $table->unique(['transaction_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+};
